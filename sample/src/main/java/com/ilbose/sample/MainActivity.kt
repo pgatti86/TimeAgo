8bc29f4fc674ef93891b73dda6ab50b99bbfe9d0
Riccardo Boselli <boselli.riccardo97@gmail.com>
2019-09-20 22:31:11 +0200

    feat: add app sample project

---
+++ sample/src/main/java/com/ilbose/sample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ilbose.sample
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by riccardo on 2019-09-20.
+ */
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
